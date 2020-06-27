7fb0c5a17040a64d9c010be34c48ea528d3f397c
ravi-maithrey-stu <66518593+ravi-maithrey-stu@users.noreply.github.com>
2020-06-27 19:35:41 +0530

    Add files via upload
    added a functionality to view followers count and pages followed

--- tweepy/twee.py
+++ tweepy/twee.py
@@ -8,3 +8,9 @@ api = tweepy.API(auth)
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
+
+user = api.get_user('bijoy78442457')
+print(user.screen_name)
+print(user.followers_count)
+for friend in user.friends():
+    print(friend.screen_name)
