dd24e603596a447fc7862e91119aed3c3a8490e6
ravi-maithrey-stu <66518593+ravi-maithrey-stu@users.noreply.github.com>
2020-06-27 20:55:21 +0530

    Update redd.py
    removed secrets, lol

--- tweepy/redd.py
+++ tweepy/redd.py
@@ -1,5 +1,5 @@
 import praw
-reddit = praw.Reddit(client_id = "WfBi7pJ4J9LwUA", client_secret = "ySGhjPD-jNXSwjwkp38jERqR6Jc", user_agent = "scra")#creting an instance of the api
+reddit = praw.Reddit(client_id = "$CLIENT_ID", client_secret = "$CLIENT_SECRET", user_agent = "$USER_AGENT")#creting an instance of the api
 new_posts = reddit.subreddit("ComedyNecrophilia").new(limit = 1)#selecting top 10 posts when sorted by new
 new_txt = open("new.txt", "w")
 for post in new_posts:#iterating and printing through them
