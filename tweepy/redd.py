dc598797df947a0d7ac3e05a60a3dadaa6bcad29
ravi-maithrey-stu <66518593+ravi-maithrey-stu@users.noreply.github.com>
2020-06-27 21:15:03 +0530

    Update redd.py

--- tweepy/redd.py
+++ tweepy/redd.py
@@ -1,6 +1,6 @@
 import praw
 reddit = praw.Reddit(client_id = "$CLIENT_ID", client_secret = "$CLIENT_SECRET", user_agent = "$USER_AGENT")#creting an instance of the api
-new_posts = reddit.subreddit("ComedyNecrophilia").new(limit = 1)#selecting top 10 posts when sorted by new
+new_posts = reddit.subreddit("SubredditName").new(limit = 1)#selecting top post when sorted by new
 new_txt = open("new.txt", "w")
 for post in new_posts:#iterating and printing through them
     new_txt.writelines(str(post.title))
